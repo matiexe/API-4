dd64a9b93b69a35129f2f455922243fd74c4ccdc
mexxtz <matiexe31@gmail.com>
2021-06-28 14:03:28 -0300

    se agrega metodo para calcular precio final

--- src/Inventario.java
+++ src/Inventario.java
@@ -12,7 +12,23 @@ public class Inventario {
     public void informarPrecios(){
         for (Producto producto : this.listaProductos) {
             System.out.println(
-                "Precio de venta para "+ producto.getNombre() +" :$"+ producto.getPrecio());
+                "Precio de venta para "+ producto.getNombre() +" :$"+ calcularPrecioVenta(producto.getPrecio(), producto.getStock()));
         }
+    }
+    private double calcularPrecioVenta(double precioCompra, int stock){
+        double precioFinal;
+        double ganancia;
+        double iva;
+
+        iva = precioCompra*0.21;
+        if((precioCompra>1500 && stock<1500) || stock <50){
+            ganancia = precioCompra*0.15;
+        }
+        else{
+            ganancia = precioCompra*0.20;
+        }
+        precioFinal = precioCompra+ganancia+iva;
+
+        return precioFinal;
     }
 }
